8373ed50e1713824981255a087cb8879789255cd
Angus Ollier <angus.ollier@outlook.com>
2022-09-25 15:48:48 +0100

    Added Bankruptcy, board emojis and fixed prices and rents.

--- js/modules/game.js
+++ js/modules/game.js
@@ -12,39 +12,40 @@ class Game {
         this.player1 = new Player("Rowan", 1)
         this.player2 = new Player("Michelle", 2)
         this.player3 = new Player("John", 3)
+        this.bankruptPlayer = null
         this.playerPositions = [0, 0, 0, 0]
 
         const go = new Location("go")
         const oldKent = new Property("old-kent", "Old Kent Rd", 2, 60)
         const whiteChapel = new Property("whitechapel", "Whitechapel Rd", 4, 60)
-        const kings = new Property("kings-cross", "Kings Cross", 4, 60)
-        const angel = new Property("angel", "The Angel Islington", 4, 60)
-        const euston = new Property("euston", "Euston Rd", 4, 60)
-        const pentonville = new Property("pentonville", "Pentonville Rd", 4, 60)
+        const kings = new Property("kings-cross", "Kings Cross", 50, 200)
+        const angel = new Property("angel", "The Angel Islington", 6, 100)
+        const euston = new Property("euston", "Euston Rd", 6, 100)
+        const pentonville = new Property("pentonville", "Pentonville Rd", 8, 120)
         //
         const jail = new Location("jail")
-        const pallMall = new Property("pall-mall", "Pall Mall", 4, 60)
-        const whitehall = new Property("whitehall", "Whitehall", 4, 60)
-        const northumberland = new Property("northumberland", "Northumberland Av", 4, 60)
-        const bow = new Property("bow", "Bow Street", 4, 60)
-        const marlborough = new Property("marlborough", "Marborough Street", 4, 60)
-        const vine = new Property("vine", "Vine Street", 4, 60)
+        const pallMall = new Property("pall-mall", "Pall Mall", 10, 140)
+        const whitehall = new Property("whitehall", "Whitehall", 10, 140)
+        const northumberland = new Property("northumberland", "Northumberland Av", 12, 160)
+        const bow = new Property("bow", "Bow Street", 14, 180)
+        const marlborough = new Property("marlborough", "Marlborough Street", 14, 180)
+        const vine = new Property("vine", "Vine Street", 20, 200)
         //
         const parking = new Location("parking")
-        const strand = new Property("strand", "Strand", 4, 60)
-        const fleet = new Property("fleet", "Fleet St", 4, 60)
-        const trafalgar = new Property("trafalgar", "Trafalgar Sq", 4, 60)
-        const leicester = new Property("leicester", "Leicester Sq", 4, 60)
-        const coventry = new Property("coventry", "Coventry St", 4, 60)
-        const piccadilly = new Property("piccadilly", "piccadilly", 4, 60)
+        const strand = new Property("strand", "Strand", 22, 220)
+        const fleet = new Property("fleet", "Fleet St", 22, 220)
+        const trafalgar = new Property("trafalgar", "Trafalgar Sq", 24, 240)
+        const leicester = new Property("leicester", "Leicester Sq", 26, 260)
+        const coventry = new Property("coventry", "Coventry St", 26, 260)
+        const piccadilly = new Property("piccadilly", "piccadilly", 28, 280)
         //
         const goToJail = new Location("go-to-jail")
-        const regent = new Property("regent", "Regent St", 4, 60)
-        const oxford = new Property("oxford", "Oxford St", 4, 60)
-        const bond = new Property("bond", "Bond St", 4, 60)
-        const fenchurch = new Property("fenchurch", "Fenchurch St", 4, 60)
-        const parkLane = new Property("park", "Park Lane", 4, 60)
-        const mayfair = new Property("mayfair", "Mayfair", 4, 60)
+        const regent = new Property("regent", "Regent St", 30, 300)
+        const oxford = new Property("oxford", "Oxford St", 30, 300)
+        const bond = new Property("bond", "Bond St", 32, 320)
+        const fenchurch = new Property("fenchurch", "Fenchurch St", 50, 200)
+        const parkLane = new Property("park", "Park Lane", 35, 350)
+        const mayfair = new Property("mayfair", "Mayfair", 40, 400)
 
         this.propertyPositions = [go, oldKent, whiteChapel, kings, angel, euston, pentonville,
             jail, pallMall, whitehall, northumberland, bow, marlborough, vine,
@@ -57,15 +58,8 @@ class Game {
         return (this.rollCounter - 1) % 4
     }
 
-    bankrupt() {
-        console.log("bankrupt")
-    }
-
     takeTurn() {
         const playerId = this.turnFinder()
-        if (this.bank.getBalance(playerId) < 1) {
-            bankrupt()
-        }
         const roll = this.dice.roll()
         const oldSquare = this.propertyPositions[this.playerPositions[playerId]]
         const playerPosition = (this.playerPositions[playerId] + roll) % this.propertyPositions.length
@@ -80,6 +74,10 @@ class Game {
             else {
                 this.payRent(playerId, square)
             }
+        }
+        if (this.bank.getBalance(playerId) < 1) {
+            console.log("a player is bankrupt")
+            this.bankruptPlayer = playerId
         }
         return { playerId: playerId, oldSquare: oldSquare.id, newSquare: square.id }
     }
